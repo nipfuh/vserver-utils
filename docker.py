560ed41e7461bb76f10f8ca06ccabb79d1764b66
Nils Fuhler <nils.fuhler@web.de>
2022-03-25 13:49:38 +0100

    fixed: wrong access to walkdir objects

--- docker.py
+++ docker.py
@@ -5,17 +5,17 @@ import config
 
 def update_images():
 
-    for subdir in os.walk(config.DOCKER_ROOT_DIR):
-        if subdir.dirpath == config.DOCKER_ROOT_DIR:
+    for root, dirs, files in os.walk(config.DOCKER_ROOT_DIR):
+        if root == config.DOCKER_ROOT_DIR:
             continue
         
         #error check or fail -> cron mail
-        os.chdir(subdir.dirpath)
+        os.chdir(root)
         pull = subprocess.run(["docker-compose", "pull"], capture_output=True, text=True, check=True)
         up = subprocess.run(["docker-compose", "up", "-d"], capture_output=True, text=True, check=True)
         #Output logic here
         #raise exception in case of error -> will be sent by cron
-        print(f'Service: {subdir.dirpath}:')
+        print(f'Service: {root}:')
         print(f'Pull: Returncode: {pull.returncode}')
         print(f'STDOUT:\n{pull.stdout}')
         print(f'----------------------------\nSTDERR:\n{pull.stderr}\n:::::::::::::::::::::::::::::::::::::::::::::::::::::')
@@ -26,13 +26,13 @@ def update_images():
 def down_containers(name=None):
     
     if not name:
-        for subdir in os.walk(config.DOCKER_ROOT_DIR):
-            if subdir.dirpath == config.DOCKER_ROOT_DIR:
+        for root, dirs, files in os.walk(config.DOCKER_ROOT_DIR):
+            if root == config.DOCKER_ROOT_DIR:
                 continue
             
-            os.chdir(subdir.dirpath)
+            os.chdir(root)
             down = subprocess.run(["docker-compose", "down"], capture_output=True, text=True, check=True)
-            print(f'Service: {subdir.dirpath}: Returncode: {down.returncode}\n')
+            print(f'Service: {root}: Returncode: {down.returncode}\n')
             print(f'STDOUT:\n{down.stdout}')
             print(f'----------------------------\nSTDERR:\n{pull.stderr}\n\n==========================================\n\n')
     else:
